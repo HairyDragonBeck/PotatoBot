81f20644a1a832f16b6e1d253bf5baad2363e211
HairyDragonBeck <hairybeck2@gmail.com>
2017-06-14 19:10:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const prefix = "<";
 
+
+client.on(ready, () => {
+    console.log('Potato Bot open and ready for duty, captn!');
+});
 client.on('message', msg => {
 
 });
+
+
 
 bot.login('');
