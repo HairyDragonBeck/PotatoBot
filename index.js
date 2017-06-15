a6fd3088b458c901c39405ca85958bf0430fa0ac
HairyDragonBeck <hairybeck2@gmail.com>
2017-06-14 20:15:56 -0400

    Added github command

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const prefix = "<";
 
+function commandIs(str, msg) {
+    return msg.content.toLowerCase()(prefix + str);
+}
 
-client.on(ready, () => {
+
+bot.on('ready', () => {
     console.log('Potato Bot open and ready for duty, captn!');
 });
-client.on('message', msg => {
+bot.on('message', msg => {
+    var args = msg.content.split(/[ ]+/);
+        if (commandIs("github", msg)) {
+            msg.reply('https://github.com/HairyDragonBeck/PotatoBot :potato:')
+        }
+
 
 });
 
