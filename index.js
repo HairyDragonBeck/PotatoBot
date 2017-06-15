439aa7263751e6b6de7174f4b26ff7e16a4c155e
HairyDragonBeck <hairybeck2@gmail.com>
2017-06-14 20:58:25 -0400

    Added clearlogs and ping command (Not complete)

--- index.js
+++ index.js
@@ -10,12 +10,26 @@ function commandIs(str, msg) {
 bot.on('ready', () => {
     console.log('Potato Bot open and ready for duty, captn!');
 });
+
 bot.on('message', msg => {
-    var args = msg.content.split(/[ ]+/);
+    let delm = m => m.delete(20000);
+    let args = msg.content.split(' ').slice(1);
         if (commandIs("github", msg)) {
-            msg.reply('https://github.com/HairyDragonBeck/PotatoBot :potato:')
+            msg.reply('https://github.com/HairyDragonBeck/PotatoBot :potato:').then(delm);
         }
 
+        if (commandIs("clearlogs", msg)){
+            if (msg.author.username != "loltatoez") {
+                msg.channel.send('Sorry, you are not the best person ever, loltatoez!');
+                console.log(msg.author.username + " attempted to clear the logs!");
+            }
+            else {
+                
+            }
+        }
+        if (commandIs("ping", msg)) {
+            msg.channel.sendMessage(`:potato:Pong! \`${msg.editedTimestamp - msg.createdTimestamp} ms\` `);
+        }
 
 });
 
